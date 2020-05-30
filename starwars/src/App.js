a9d53733a946eee8eed1f0e350a63b736c504466
Chanis Torres <wonderland@Chaniss-MacBook-Pro.local>
2020-05-29 22:44:41 -0400

    MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios";
+import Characters from "./components/Character"
+import { Container, Row } from "reactstrap";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [character, setCharacter] = useState([])
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect (() => {
+    axios.get("https://rickandmortyapi.com/api/character")
+    .then(response => {
+     console.log('response', response)
+     setCharacter(response.data.results)
+    })
+    .catch(er => console.log("error", er));
+  }, []);
+  console.log(character);
+
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+      <Container className="App">
+        <h1 className="Header">Characters</h1>
+        <Row> 
+        {character.map(el => {
+        return <Characters {...el}/>
+        })}
+        </Row>
+     </Container>
   );
 }
 
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,32 @@
 // Write your Character component here
+
+import React from "react";
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardText,
+    Col
+  } from "reactstrap";
+
+
+const Characters = props => {
+    // console.log(props.name)
+    return(
+     <Col xs="6" md="4" xl="3">
+        <Card>
+         <CardHeader className= "name">{props.name}</CardHeader>
+         <CardBody className = "card">
+            <img src= {props.image} alt = "Image of Character"/>
+            <CardText>Origin: {props.origin.name}</CardText>
+            <CardText>Species: {props.species}</CardText>
+            <CardText>Gender: {props.gender}</CardText>
+            <CardText>Status: {props.status}</CardText>
+         </CardBody>
+        </Card>
+      </Col>
+    )
+}
+
+// status
+export default Characters
